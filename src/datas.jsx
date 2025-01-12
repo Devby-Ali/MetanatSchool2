e340e03c692a1bdb70294c9abc9fb99fc0a98df8
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-01-12 17:31:30 +0330

    Dev Landing Component

---
+++ src/datas.jsx
@@ -0,0 +1,158 @@
+let sliderImg = [
+  {
+    id: 1,
+    src: "images/BgHeader_1.jpg",
+  },
+  {
+    id: 2,
+    src: "images/BgHeader_2.jpg",
+  },
+];
+
+let classesData = [
+  {
+    id: 1,
+    img: "images/child_falsafe.jpg",
+    desc: "کارگاه فلسفه برای کودکان",
+    alt: "child_falsafeImg"
+  },
+  {
+    id: 2,
+    img: "images/honareTajasomi2.jpg",
+    desc: "هنر های تجسمی حرفه ای",
+    alt: "honareTajasomi2Img"
+  },
+  {
+    id: 3,
+    img: "images/varzesh.jpg",
+    desc: "ورزش حرفه ای فکری و عملی",
+    alt: "varzeshImg"
+  },
+  {
+    id: 4,
+    img: "images/zabanEnglishhh.jpg",
+    desc: "آموزش زبان انگلیسی",
+    alt: "zabanEnglishImg"
+  },
+  {
+    id: 5,
+    img: "images/zabanTorki.jpg",
+    desc: "آموزش زبان ترکی استانبولی",
+    alt: "zabanTorkiImg"
+  },
+  {
+    id: 6,
+    img: "images/Azmoon.jpg",
+    desc: "آزمون های پیشرفت تحصیلی",
+    alt: "AzmoonImg"
+  },
+]
+
+let CoursesData = [
+  {
+    id: 1,
+    img: "images/Azmoon.jpg",
+    title: "آزمون های منسجم",
+    desc: "برگزاری آزمون های پیشرفت تحصیلی و ارائه کارنامه آزمون",
+    alt: "coursesImg"
+  },
+  {
+    id: 2,
+    img: "images/Course_1.jpg",
+    title: "کلاس های تقویتی",
+    desc: "کلاس های خصوصی و گروهی , برگزاری دوره های کوتاه مدت آموزشی",
+    alt: "coursesImg"
+  },
+  {
+    id: 3,
+    img: "images/moshavere.jpg",
+    title: "مشاوره و برنامه ریزی تحصیلی",
+    desc: "حضور مستمر مشاورین تحصیلی , مذهبی , پزشک , کودک و خانواده",
+    alt: "coursesImg"
+  },
+]
+
+let EventsData = [
+  {
+    id: 1,
+    img: "images/EventImg.jpg",
+    title: "جشن افتتاحیه",
+    desc: "افتتاح اولین مدرسه ی مونته سوری در استان مرکزی",
+    alt: "EventImg"
+  },
+  {
+    id: 2,
+    img: "images/EventImg_2.jpg",
+    title: "پروتکل های کرونا برای مدارس",
+    desc: "با آموزش درست، می‌توان دانش‌آموزان را تشویق کرد تا با دیگران در مورد چگونگی جلوگیری از پخش ویروس‌ها صحبت کنند و مدافعین پیشگیری و کنترل بیماری در خانه، مدرسه و ...",
+    alt: "EventImg"
+  },
+  {
+    id: 3,
+    img: "images/EventImg_3.jpg",
+    title: "جشن رنگ‌ها",
+    desc: "جشنواره رنگ بازی سبب تخلیه هیجانات روحی کودکانمان است علاوه برآن زمینه ساز رشد خلاقیت و ایده پردازی در آنها می باشد وسبب شناخت رنگها است.",
+    alt: "EventImg"
+  },
+  {
+    id: 4,
+    img: "images/EventImg_4.jpg",
+    title: "روز معلم سال 1403",
+    desc: "روز معلم در ایران هر ساله در تاریخ ۱۲ اردیبهشت ماه جشن گرفته می‌شود و به منظور تجلیل و قدردانی از معلمان و تعلیم و تربیت نسل‌ها اختصاص یافته است ...",
+    alt: "EventImg"
+  },
+  {
+    id: 5,
+    img: "images/EventImg_5.jpg",
+    title: "روز دختر 1403",
+    desc: "روز دختر در ایران به مناسبت تولد حضرت فاطمه معصومه (س) و به عنوان روز دختر نام‌گذاری شده است. این روز عزیز به منظور تکریم و تقدیر از جایگاه ...",
+    alt: "EventImg"
+  },
+ 
+]
+let TeachersData = [
+  {
+    id: 1,
+    img: "images/teachers_1.jpg",
+    name: "والتر وایت",
+    job: "مربی شیمی",
+    alt: "teacherImg"
+  },
+  {
+    id: 2,
+    img: "images/teachers_2.jpg",
+    name: "میکاسا یاگامی",
+    job: "استاد فلسفه",
+    alt: "teacherImg"
+  },
+  {
+    id: 3,
+    img: "images/teachers_3.webp",
+    name: "ماریانا جنر",
+    job: "مربی هنر",
+    alt: "teacherImg"
+  },
+  {
+    id: 4,
+    img: "images/teachers_4.jpg",
+    name: "سلنا اسمیت",
+    job: "مربی زبان انگلیسی",
+    alt: "teacherImg"
+  },
+  {
+    id: 5,
+    img: "images/teachers_5.jpg",
+    name: "آیسان گوندش",
+    job: "مربی زبان ترکی",
+    alt: "teacherImg"
+  },
+  {
+    id: 6,
+    img: "images/teachers_6.jpg",
+    name: "جان پیترسون",
+    job: "مشاوره تحصیلی",
+    alt: "teacherImg"
+  },
+]
+
+export { sliderImg, classesData, CoursesData, EventsData, TeachersData };
